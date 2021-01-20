3f563d2ae1a0593da5f63b3fcb2bf5b510967f5d
JoseMMorales <josemulamorales33@hotmail.com>
2021-01-20 11:34:22 +0100

    Update .editorcofig file to React-Hook-Sass Proyect

--- src/App.js
+++ src/App.js
@@ -13,71 +13,71 @@ function App() {
 
 	return (
 		<BrowserRouter >
-		<nav className='navbar navbar-expand-lg navbar-dark fixed-top'>
-			<a 
-			className='navbar-brand' 
-			href='/pages/CardList'>
-			<img src='/img/reactJS.png' alt='react-icon'/>
-			</a>
-			<button 
-			onClick={handleNavCollapse} 
-			className='navbar-toggler' 
-			type='button' 
-			data-toggle='collapse' 
-			data-target='#newNavBar' 
-			aria-controls='newNavBar' 
-			aria-expanded={!isNavCollapsed && true}>
-			<span className='navbar-toggler-icon'/>
-			</button>
-			<div 
-			className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id='newNavBar'>
-			<ul className='navbar-nav mx-auto text-center'>
-				<li className='nav-item'>
-				<NavLink 
-					className='nav-link'
-					activeClassName='active-style' 
-					to='/pages/CardList'>
-					Card List
-				</NavLink>
-				</li>
-				<li className='nav-item'>
-				<NavLink 
-					className='nav-link' 
-					activeClassName='active-style'
-					to='/pages/ShopList'>
-					Shop List
-				</NavLink>
-				</li>
-				<li className='nav-item'>
-				<NavLink 
-					className='nav-link' 
-					activeClassName='active-style'
-					to='/pages/AgendaList'>
-					Agenda List
-				</NavLink>
-				</li>
-				<li className='nav-item'>
-				<NavLink 
-					className='nav-link' 
-					activeClassName='active-style'
-					to='/pages/ToDoList'>
-					ToDo List
-				</NavLink>
-				</li>
-			</ul>
-			</div>
-		</nav>
-		<Switch>
-			<GlobalContextProvider>
-				<Route exact path='/'>
-					<Redirect to= '/pages/CardList'/>
-				</Route>
-				<Route exact path= '/pages/CardList' component={CardList}/>
-				<Route path='/pages/ShopList' component={ShopList}/>
-				<Route path='/pages/AgendaList' component={AgendaList}/>
-				<Route path='/pages/ToDoList' component={ToDoList}/>
-			</GlobalContextProvider>
-		</Switch>
+      <nav className='navbar navbar-expand-lg navbar-dark fixed-top'>
+        <a
+          className='navbar-brand'
+          href='/pages/CardList'>
+          <img src='/img/reactJS.png' alt='react-icon'/>
+        </a>
+        <button
+          onClick={handleNavCollapse}
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#newNavBar'
+          aria-controls='newNavBar'
+          aria-expanded={!isNavCollapsed && true}>
+        <span className='navbar-toggler-icon'/>
+        </button>
+        <div
+          className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id='newNavBar'>
+          <ul className='navbar-nav mx-auto text-center'>
+            <li className='nav-item'>
+            <NavLink
+              className='nav-link'
+              activeClassName='active-style'
+              to='/pages/CardList'>
+              Card List
+            </NavLink>
+            </li>
+            <li className='nav-item'>
+            <NavLink
+              className='nav-link'
+              activeClassName='active-style'
+              to='/pages/ShopList'>
+              Shop List
+            </NavLink>
+            </li>
+            <li className='nav-item'>
+            <NavLink
+              className='nav-link'
+              activeClassName='active-style'
+              to='/pages/AgendaList'>
+              Agenda List
+            </NavLink>
+            </li>
+            <li className='nav-item'>
+            <NavLink
+              className='nav-link'
+              activeClassName='active-style'
+              to='/pages/ToDoList'>
+              ToDo List
+            </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Switch>
+        <GlobalContextProvider>
+          <Route exact path='/'>
+            <Redirect to= '/pages/CardList'/>
+          </Route>
+          <Route exact path= '/pages/CardList' component={CardList}/>
+          <Route path='/pages/ShopList' component={ShopList}/>
+          <Route path='/pages/AgendaList' component={AgendaList}/>
+          <Route path='/pages/ToDoList' component={ToDoList}/>
+        </GlobalContextProvider>
+      </Switch>
 		</BrowserRouter>
 	);
 }
